9a345f792684fa8bb0414707c1e539446b4558c6
Akash <105856031+akashbhor1356@users.noreply.github.com>
2023-12-10 10:26:01 +0530

    flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,28 +1,54 @@
-from flask import Flask, request
-import mysql.connector
+from flask import Flask, request, jsonify
+from torchvision import models, transforms
+from PIL import Image
+import torch
+import numpy as np
 
 app = Flask(__name__)
 
-# MySQL connection configuration
-db = mysql.connector.connect(
-    host="localhost",
-    user="username",
-    password="password",
-    database="prediction_logs"
-)
-cursor = db.cursor()
+# Load the saved model
+model = models.resnet18(pretrained=True)
+# ... (Rest of your model setup code)
 
+# Define transformations for incoming images
+preprocess = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+# Route for predicting classes
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json()
-    # Perform prediction using the trained model
-    # prediction = model.predict(data)
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file found'})
     
-    # Log prediction into MySQL database
-    cursor.execute("INSERT INTO predictions (input_data, prediction) VALUES (%s, %s)", (str(data), str(prediction)))
-    db.commit()
+    file = request.files['file']
     
-    return "Prediction logged successfully"
+    # Ensure the file is an image
+    if file.filename == '':
+        return jsonify({'error': 'No file selected'})
+    
+    if file:
+        try:
+            image = Image.open(file.stream)
+            input_tensor = preprocess(image)
+            input_batch = input_tensor.unsqueeze(0)  # Add a batch dimension
+            
+            # Make predictions
+            outputs = model(input_batch)
+            _, preds = torch.max(outputs, 1)
+            
+            predicted_class = preds.item()  # Get the predicted class index
+            class_names = ['daisy', 'dandelion']  # Define your class names here
+            
+            predicted_class_name = class_names[predicted_class]
+            
+            return jsonify({'predicted_class': predicted_class_name})
+        except Exception as e:
+            return jsonify({'error': str(e)})
+    return jsonify({'error': 'Invalid file'})
 
 if __name__ == '__main__':
     app.run(debug=True)
